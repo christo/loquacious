02a8e84e22bbc9b0fa4aa6f566f3494edcf250e5
Chris Mountford <chris.mountford@gmail.com>
2024-10-17 17:36:55 +1100

    fixed bug where invite mode was not being used!

--- server/src/Modes.ts
+++ server/src/Modes.ts
@@ -2,6 +2,8 @@ import {readFileSync} from "fs";
 import {OpenAI} from "openai";
 import type {SpeechSystem} from "speech/SpeechSystem";
 
+// TODO need a warmup mode for any pre-operational work
+
 const chatModeSystemPrompt: string = readFileSync("prompts/fortune-system-prompt.txt").toString();
 const inviteModeSystemPrompt: string = readFileSync("prompts/invite-mode.prompt.txt").toString();
 const universalSystemPrompt: string = readFileSync("prompts/universal-system.prompt.txt").toString();
@@ -18,43 +20,57 @@ class Modes {
   constructor() {
   }
 
+  /**
+   * Gives system instructions for how to render a pause for the given speech system.
+   * @param ss
+   */
   pauseInstructions(ss: SpeechSystem) {
     const cmd = ss.pauseCommand(1000);
     if (cmd == null) {
-      // no explicit pause command
-      return "You frequently pause for dramatic effect. To do so, include elipsis and/or literally this: `\\n\\n`"
+      // no explicit pause command syntax
+      return "You frequently pause for dramatic effect. To do so, include elipsis and/or two new lines: `\\n\\n`"
     } else {
       return `You frequently pause for dramatic effect. To do so you always use precise syntax specifying the length of
   pause. A one second (1000 millisecond) pause must be specified exactly like this: \`${cmd}\``;
     }
   }
 
+  /**
+   * Two-sided conversation, next step initiated by the given prompt, renderd for the given
+   * {@link SpeechSystem}.
+   *
+   * @param prompt
+   * @param ss
+   */
   chatModeMessages(prompt: string, ss: SpeechSystem): OpenAI.Chat.Completions.ChatCompletionMessageParam[] {
+    const systemParts = [
+      dateTimePrompt(),
+      chatModeSystemPrompt,
+      universalSystemPrompt,
+      this.pauseInstructions(ss)
+    ].join("\\n\\n")
     return [
-      {
-        role: 'system', content: `${dateTimePrompt()}
-      
-      ${chatModeSystemPrompt}
-      
-      ${universalSystemPrompt}
-      
-      ${this.pauseInstructions(ss)}`
-      },
+      {role: 'system', content: systemParts},
       {role: 'user', content: prompt}
     ];
   }
 
+  /**
+   * Initiated by us to invite a conversation.
+   *
+   * @param prompt ignored.
+   * @param ss speech system to render for.
+   */
   inviteModeMessages(prompt: string, ss: SpeechSystem): OpenAI.Chat.Completions.ChatCompletionMessageParam[] {
-    return [{
-      role: 'system',
-      content: `${dateTimePrompt()}
-      
-      ${chatModeSystemPrompt}
-      
-      ${universalSystemPrompt}
-      
-      ${this.pauseInstructions(ss)}`
-    }];
+
+    const systemPrompt = [
+      dateTimePrompt(),
+      inviteModeSystemPrompt,
+      universalSystemPrompt,
+      this.pauseInstructions(ss)
+    ].join("\\n\\n");
+
+    return [{role: 'system', content: systemPrompt}];
   }
 }
 
