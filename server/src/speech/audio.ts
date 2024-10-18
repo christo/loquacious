d274dd8b917acf4575a05054f0f770a36af016a7
Chris Mountford <chris.mountford@gmail.com>
2024-10-18 12:21:04 +1100

    add video formats, introduce type

--- server/src/speech/audio.ts
+++ server/src/speech/audio.ts
@@ -1,3 +1,7 @@
+/**
+ * @param format
+ * @deprecated use MediaFormat instances
+ */
 function formatToMimeType(format: SupportedAudioFormat) {
   switch (format) {
     case 'aiff':
@@ -11,10 +15,74 @@ function formatToMimeType(format: SupportedAudioFormat) {
   }
 }
 
+const FORMATS: MediaFormat[] = [
+  {
+    extensions: ["mp3"],
+    mimeType: "audio/mp3",
+    modality: "audio"
+  },
+  {
+    extensions: ["aiff"],
+    mimeType: "audio/aiff",
+    modality: "audio"
+  },
+  {
+    extensions: ["wav"],
+    mimeType: "audio/wav",
+    modality: "audio"
+  },
+  {
+    extensions: ["mp4"],
+    mimeType: "video/mp4",
+    modality: "video"
+  }
+]
+
+type FormatByString = { [key: string]: MediaFormat };
+const FORMAT_BY_EXT: FormatByString = FORMATS.reduce((m, f) => {
+  f.extensions.forEach(ext => m[ext] = f);
+  return m;
+}, ({} as FormatByString));
+const FORMAT_BY_MIME_TYPE: FormatByString = FORMATS.reduce((m, f) => {
+  m[f.mimeType] = f;
+  return m;
+}, ({} as FormatByString));
+
+/**
+ * Derives the {@link MediaFormat} for the given filepath or extension.
+ * @param fpe a filepath ending in dot extension or just the extension
+ * @return corresponding {@link MediaFormat} or undefined if unknown
+ */
+function extToFormat(fpe: string): MediaFormat | undefined {
+  const dotSplit = fpe.split('.');
+  const ext = dotSplit[dotSplit.length - 1];
+  return FORMAT_BY_EXT[ext];
+}
+
+/**
+ * Derives the {@link MediaFormat} for the given Mime-Type.
+ * @param mimeType a filepath ending in dot extension or just the extension
+ * @return corresponding {@link MediaFormat} or undefined if unknown
+ */
+function mimeTypeToFormat(mimeType: string): MediaFormat | undefined {
+  return FORMAT_BY_MIME_TYPE[mimeType];
+}
+
+/** @deprecated use MediaFormat instances */
 const TYPE_MP3 = "mp3"
+/** @deprecated use MediaFormat instances */
 const TYPE_AIFF = "aiff"
+/** @deprecated use MediaFormat instances */
 const TYPE_WAV = "wav"
+/** @deprecated use MediaFormat instances */
 const TYPE_DEFAULT = TYPE_MP3;
 
+type MediaFormat = {
+  extensions: string[];
+  mimeType: string;
+  modality: "image" | "video" | "audio";
+}
+
+/** @deprecated use MediaFormat instances */
 export type SupportedAudioFormat = "aiff" | "mp3" | "wav";
-export {TYPE_AIFF, TYPE_MP3, TYPE_WAV, TYPE_DEFAULT, formatToMimeType};
+export {TYPE_AIFF, TYPE_MP3, TYPE_WAV, TYPE_DEFAULT, formatToMimeType, type MediaFormat, extToFormat, mimeTypeToFormat};
