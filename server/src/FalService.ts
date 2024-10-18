0eef562e9e9eb8895edda2e09416a2d95a62749d
Chris Mountford <chris.mountford@gmail.com>
2024-10-19 09:38:37 +1100

    rewrite url cache

--- server/src/FalService.ts
+++ server/src/FalService.ts
@@ -53,6 +53,7 @@ async function readBinaryFile(filePath: string): Promise<File> {
 class FalService implements LipSync {
   private static SADTALKER_ENDPOINT: string = "fal-ai/sadtalker";
   private readonly lipSyncDataDir: string;
+  private urlCache : {[keyOf: string]: string} = {};
 
   constructor(lipSyncDataDir: string) {
     this.lipSyncDataDir = lipSyncDataDir;
@@ -61,15 +62,22 @@ class FalService implements LipSync {
     });
   }
 
-  async lipSync(img: string, speech: string): Promise<LipSyncResult> {
-    const imgFile = await readBinaryFile(img);
-    const imgUrl = await timed<string>("fal upload image", () => fal.storage.upload(imgFile));
-    const speechFile = await readBinaryFile(speech);
-    const speechUrl = await timed<string>("fal upload speech", () => fal.storage.upload(speechFile));
+  async urlFor(filePath: string): Promise<string> {
+    const fileUrl = this.urlCache[filePath];
+    if (!fileUrl) {
+      await timed("fal upload image", async () => {
+        this.urlCache[filePath] = await fal.storage.upload(await readBinaryFile(filePath));
+      });
+    }
+    return this.urlCache[filePath];
+  }
 
+  async lipSync(img: string, speech: string): Promise<LipSyncResult> {
+    const imgUrl = await this.urlFor(img);
+    const speechUrl = await this.urlFor(speech);
     const result: Result<{ video: SadTalkerLipSyncResult }> = await timed(
       "fal run sadtalker",
-      () => fal.run(FalService.SADTALKER_ENDPOINT, this.sadtalkerParams(imgUrl, speechUrl)));
+      async () => await fal.run(FalService.SADTALKER_ENDPOINT, this.sadtalkerParams(imgUrl, speechUrl)));
     return timed("fal sadtalker video download", async () => {
       const r = result.data.video;
       const videoDownload = await fetch(r.url);
@@ -85,11 +93,11 @@ class FalService implements LipSync {
       input: {
         source_image_url: imgUrl,
         driven_audio_url: speechUrl,
-        pose_style: 14,             // 0-45 integer
-        face_model_resolution: "512",    // string "256" or "512"
+        pose_style: 24,             // 0-45 integer
+        face_model_resolution: "256",    // string "256" or "512"
         expression_scale: 1.4,      // 0-3 float
         //face_enhancer: 'gfpgan',       // blank or only option - not sure of impact
-        still_mode: false,             // whether to use few head movements
+        still_mode: true,             // whether to use few head movements
         preprocess: "extfull",            // crop, extcrop, resize, full, extfull
       }
     };
