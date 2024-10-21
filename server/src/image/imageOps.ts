b5c4162a2b956d4bcec55959e7dde86a8c70375a
Chris Mountford <chris.mountford@gmail.com>
2024-10-21 13:20:38 +1100

    make image resizer

---
+++ server/src/image/imageOps.ts
@@ -0,0 +1,36 @@
+import {promises as fs} from "fs";
+import {type MediaFormat, supportedImageTypes} from "media";
+import path from "path";
+import sharp from "sharp";
+import {mkDirIfMissing} from "../system/config";
+
+type Dim = {
+  width: number,
+  height: number
+}
+
+export async function prescaleImages(baseDir: string, dimensions: Dim[]) {
+  const extensions: string[] = supportedImageTypes()
+    .flatMap((f: MediaFormat) => f.extensions)
+    .map((e: string) => `.${e}`);
+  for (const dimension of dimensions) {
+    const dimDir = `${baseDir}/${dimension.width}x${dimension.height}`;
+    console.log(`prescaling images to ${dimDir}`);
+    mkDirIfMissing(dimDir);
+    const files = await fs.readdir(baseDir, {withFileTypes: true});
+    for (const file of files) {
+      if (file.isFile() && extensions.includes(path.extname(file.name).toLowerCase())) {
+        // got suitable image file, resize it
+        const destFile = path.join(dimDir, file.name);
+        // skip if the file already exists
+        fs.access(destFile, fs.constants.R_OK | fs.constants.W_OK).then(null, async () => {
+          await sharp(path.join(baseDir, file.name))
+            .resize(dimension.width, dimension.height, {fit: "contain"})
+            .toFile(destFile)
+        });
+      }
+    }
+  }
+}
+
+export type {Dim};
