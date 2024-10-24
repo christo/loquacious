39ae3e4940352c9fcb72f41d3194223694b694d6
Chris Mountford <chris.mountford@gmail.com>
2024-10-24 13:36:21 +1100

    make fakellm more useful

--- server/src/llm/FakeLlm.ts
+++ server/src/llm/FakeLlm.ts
@@ -3,30 +3,68 @@ import OpenAI from "openai";
 import ChatCompletionMessageParam = OpenAI.ChatCompletionMessageParam;
 import Model = OpenAI.Model;
 
+/*
+Using acouple of OpenAI's lightweight interfaces as part of our public interface seems fine.
+ */
+
+
+/** Support multiple "models" to conveniently switch between different fake behaviour */
+class FakeModel implements Model {
+  created: number;
+  id: string;
+  object: "model";
+  owned_by: string;
+  chat: (params: ChatCompletionMessageParam[]) => ChatResult;
+
+  constructor(id: string, fn: (params: ChatCompletionMessageParam[]) => ChatResult) {
+    this.created = 0;
+    this.id = id;
+    this.object = "model";
+    this.owned_by = "loquacious";
+    this.chat = fn;
+  }
+}
+
+const KM_STATIC: string = "static";
+const KM_ECHO: string = "echo";
+const KM_CLOCK: string = "clock";
+
+function dateTimeMessage() {
+  const now = new Date();
+  return `The date is ${now.toLocaleDateString()} and the time is ${now.toLocaleTimeString()}`;
+}
+
+/**
+ * For testing.
+ */
 class FakeLlm implements Llm {
   baseUrl = "";
-  name = "Fake Llm"
+  name = "FakeLlm"
   enableHealth = false;
+  currentModelKey = KM_ECHO;
 
-  private mymodel = {
-    id: "fake",
-    created: Date.now(),
-    object: 'model',
-    owned_by: "christo"
+  private readonly myModels: { [key: string]: FakeModel; } = {
+    KM_STATIC: new FakeModel("static", (params: ChatCompletionMessageParam[]) => ({message: "fake chat result"})),
+    KM_ECHO: new FakeModel("echo", (params: ChatCompletionMessageParam[]) => {
+      try {
+        return {message: (params!.filter(mp => mp.role === "user")!.pop())!.content.toString()!}
+      } catch (err) {
+        return {message: `I tried to be fake but I failed. Here's how it went down: ${err}`}
+      }
+    }),
+    KM_CLOCK: new FakeModel("static", (params: ChatCompletionMessageParam[]) => ({message: dateTimeMessage()})),
   }
 
-  private readonly result: ChatResult = {message: "fake chat result"};
-
   chat(_params: ChatCompletionMessageParam[]): Promise<ChatResult> {
-    return Promise.resolve(this.result);
+    return Promise.resolve(this.myModels[this.currentModelKey].chat(_params));
   }
 
   currentModel(): Promise<string> {
-    return Promise.resolve(this.mymodel.id);
+    return Promise.resolve(this.myModels[this.currentModelKey].id);
   }
 
   models(): Promise<Array<Model>> {
-    return Promise.resolve([this.mymodel as Model]);
+    return Promise.resolve(Object.values(this.myModels));
   }
 }
 
