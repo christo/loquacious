3a32c37f90b3c50718ed939a762fe091d7e6a631
Chris Mountford <chris.mountford@gmail.com>
2024-11-07 01:15:44 +1100

    todo clarify

--- server/src/speech/MacOsSpeech.ts
+++ server/src/speech/MacOsSpeech.ts
@@ -156,7 +156,8 @@ class MacOsSpeech implements SpeechSystem {
   }
 
   configure(metadata: string): Promise<void> {
-    // TODO implement available voice detection somehow. the say command lists voices not yet installed
+    // TODO implement available voice detection somehow. the say command lists voices not yet installed and calling say
+    //      with a missing voice produces no audio (IIRC)
     let found = false;
     for (let i = 0; i < VOICES.length; i++) {
       if (VOICES[i].voiceId === metadata) {
