bd3a0960ef8fc64091f2ac8d838fc63793831833
Chris Mountford <chris.mountford@gmail.com>
2024-12-05 15:11:37 +1100

    jsdoc

--- server/src/domain/SystemSummary.ts
+++ server/src/domain/SystemSummary.ts
@@ -2,12 +2,18 @@ import {SpeechSystemOption} from "../speech/SpeechSystems";
 import {RunInfo} from "./RunInfo";
 import {LlmModel} from "../llm/LlmModel";
 
+/**
+ * Element of the system that could be replaced by an alternative.
+ */
 interface Module {
   current: string;
   all: string[];
   isFree: boolean;
 }
 
+/**
+ * {@link Module} that has user-choosable options.
+ */
 interface ModuleWithOptions<T> extends Module {
   options: T[];
   currentOption: T;
