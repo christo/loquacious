6b76b3cb48fef213207609d97c9e8558f9521d4a
Chris Mountford <chris.mountford@gmail.com>
2024-12-11 18:40:17 +1100

    dead code

--- server/src/api/mediaStream.ts
+++ server/src/api/mediaStream.ts
@@ -42,7 +42,6 @@ export function fileStream(filename: string | undefined, res: Response) {
   if (!filename) {
     res.status(404).json({message: "No filename given"});
   } else {
-    // console.log(`got request for file ${filename}`);
     streamFromPath(filename, res);
   }
 }
