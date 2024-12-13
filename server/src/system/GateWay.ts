8c5d836f899b29eab9eb296c4a02d4479de3a26f
Chris Mountford <chris.mountford@gmail.com>
2024-12-13 11:20:25 +1100

    todo

--- server/src/system/GateWay.ts
+++ server/src/system/GateWay.ts
@@ -1,5 +1,6 @@
 import type {CreatorService} from "./CreatorService";
 
+// TODO make use of this for all such services: speech, lipsync...
 export interface GateWay<T extends CreatorService> {
   current(): T;
 
